ede19b80fdf32d70b9a27c17867b135d7e4799a3
Ubuntu <ubuntu@ip-172-31-46-235.ap-south-1.compute.internal>
2025-02-28 07:49:42 +0000

    This is my first script

---
+++ day1/hello.sh
@@ -0,0 +1,5 @@
+echo "Reporter: Hey, what's up!"
+echo "Audience: Yeah,We are good ! How About You!"
+echo "Reporter: What is the most challenging thing you faced this year?"
+echo "Audience: You! hahaha! No sense of questions you do have  to ask to audience"
+
