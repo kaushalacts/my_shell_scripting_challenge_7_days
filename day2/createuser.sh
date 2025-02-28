d8d2562a1aa9dc696175da8787d45b7d57e88fa4
Ubuntu <ubuntu@ip-172-31-46-235.ap-south-1.compute.internal>
2025-02-28 11:12:49 +0000

    day2 scripting Challenge

---
+++ day2/createuser.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+ read -p "Enter username" username 
+ echo "You entered $username"
+ sudo useradd -m $username
+ echo "New User added "
---
+++ day2/jetha_lal_ki_duniya.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#This is Jetha Lal Ki duniya 
+<< Disclaimer 
+Any thing written here will be pe printed , only comment. 
+
+Disclaimer
+
+name="Jetha"
+echo "Name is $name"
+
+echo " Enter the name" 
+read username
+
+echo "You entered $username"
